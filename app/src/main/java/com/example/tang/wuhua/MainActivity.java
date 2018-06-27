9d211ac4fbf8c48de3fd80dda3bf1a9b9a8a1ec4
sifeng666 <sifeng666@github.com>
2018-06-27 22:15:02 +0800

    把act_test改为activity_main，完善了header页面

--- app/src/main/java/com/example/tang/wuhua/MainActivity.java
+++ app/src/main/java/com/example/tang/wuhua/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_act_test);
+        setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
         mLocationClient = new LocationClient(getApplicationContext());
