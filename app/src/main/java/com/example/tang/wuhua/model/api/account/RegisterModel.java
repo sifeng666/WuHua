6dcc987d7952552b46775e56d8b6b1646fbe3e55
z1ycheng <gravity_temptation@outlook.com>
2018-07-01 03:37:47 +0800

    修改model目录下的实体 新增网络模块net 添加项目中的持久化常量Constant

--- app/src/main/java/com/example/tang/wuhua/model/api/account/RegisterModel.java
+++ app/src/main/java/com/example/tang/wuhua/model/api/account/RegisterModel.java
@@ -1,16 +1,13 @@
 package com.example.tang.wuhua.model.api.account;
 
 /**
- * 用户注册请求的实体类
+ * 用户注册请求的实体类参数
  * @author z1ycheng
  */
 
 public class RegisterModel {
-    // 用户账号
-    private String account;
-
     // 用户名
-    private String name;
+    private String username;
 
     // 密码
     private String password;
@@ -22,34 +19,25 @@ public class RegisterModel {
      * 构造方法
      */
 
-    public RegisterModel(String account, String name, String password, String pushId) {
-        this.account = account;
-        this.name = name;
+    public RegisterModel(String username, String password, String pushId) {
+        this.username = username;
         this.password = password;
         this.pushId = pushId;
     }
 
-    public RegisterModel(String account, String name, String password) {
-        this(account, name, password, null);
+    public RegisterModel(String username, String password) {
+        this(username, password, null);
     }
 
     /**
      * getter and setter
      */
-    public String getAccount() {
-        return account;
+    public String getUsername() {
+        return username;
     }
 
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
