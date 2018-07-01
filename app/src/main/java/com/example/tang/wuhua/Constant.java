0999cfea7188291969b747fa7424a834c52b9015
z1ycheng <gravity_temptation@outlook.com>
2018-07-01 18:32:13 +0800

    添加百度api的url、ak以及安全码

--- app/src/main/java/com/example/tang/wuhua/Constant.java
+++ app/src/main/java/com/example/tang/wuhua/Constant.java
@@ -13,7 +13,17 @@ public interface Constant {
         // 乐帮的url
         // String BASE_URL = "http://139.199.9.244:8082/";
 
+        // 百度api的url
+        String BAIDU_API_URL = "http://api.map.baidu.com/";
+
+
         // 电话号码的正则
         String REGEX_PHONENUM = "[1][3,4,5,7,8][0-9]{9}$";
+
+        // 百度api的ak
+        String BAIDU_API_AK = "VLmb30ftbg4nTLF5uTKqduTVtOxp7mCi";
+
+        // 百度api安全码
+        String BAIDU_API_MCODE = "D5:6E:00:F7:CC:57:F7:ED:93:71:14:0B:65:8C:60:B6:BC:2B:67:A6;com.example.tang.wuhua";
     }
 }
