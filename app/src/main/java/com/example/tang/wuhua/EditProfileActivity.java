8965d82cafbb55289ecd3daef432bb47efaff9ee
Chiyeung Chan <946041030@qq.com>
2018-07-05 00:00:25 +0800

    添加了修改个人信息的activity，设置页面添加了几个按钮的相应函数，发送页面添加获取位置信息

---
+++ app/src/main/java/com/example/tang/wuhua/EditProfileActivity.java
@@ -0,0 +1,17 @@
+package com.example.tang.wuhua;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by root on 18-7-4.
+ */
+
+public class EditProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_profile);
+    }
+}
