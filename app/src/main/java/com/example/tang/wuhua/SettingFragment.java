b8ef4db134365842c44ab9fce197d42d5b905a0d
sifeng666 <tang98@hotmail.com>
2018-07-06 12:07:50 +0800

    处理了设置页面

--- app/src/main/java/com/example/tang/wuhua/SettingFragment.java
+++ app/src/main/java/com/example/tang/wuhua/SettingFragment.java
@@ -1,18 +1,25 @@
 package com.example.tang.wuhua;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
+
+import com.afollestad.materialdialogs.GravityEnum;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.suke.widget.SwitchButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by root on 18-6-30.
@@ -20,19 +27,20 @@ import butterknife.Unbinder;
 
 public class SettingFragment extends Fragment {
 
+    private MaterialDialog.Builder mBuilder;
+    private MaterialDialog mMaterialDialog;
 
     TextView tvSwitchAccount;
     TextView tvLogout;
     TextView tvExit;
-    @BindView(R.id.layout_remind_settings)
     LinearLayout layoutRemindSettings;
-    @BindView(R.id.layout_privacy_settings)
+    LinearLayout layoutGeneralSettings;
     LinearLayout layoutPrivacySettings;
-    @BindView(R.id.layout_help_settings)
     LinearLayout layoutHelpSettings;
-    @BindView(R.id.layout_about_settings)
     LinearLayout layoutAboutSettings;
-    Unbinder unbinder;
+    SwitchButton toggleBtn;
+
+    ArrayList advList;
 
     @Nullable
     @Override
@@ -41,17 +49,61 @@ public class SettingFragment extends Fragment {
         tvSwitchAccount = v.findViewById(R.id.switch_account);
         tvLogout = v.findViewById(R.id.logout);
         tvExit = v.findViewById(R.id.exitApp);
+        layoutRemindSettings = v.findViewById(R.id.layout_remind_settings);
+        layoutGeneralSettings = v.findViewById(R.id.layout_general_settings);
+        layoutAboutSettings = v.findViewById(R.id.layout_about_settings);
+        layoutHelpSettings = v.findViewById(R.id.layout_help_settings);
+        layoutPrivacySettings = v.findViewById(R.id.layout_privacy_settings);
+        toggleBtn = v.findViewById(R.id.switch_button);
 
         bindListener();
 
-        unbinder = ButterKnife.bind(this, v);
+
         return v;
     }
 
     private void bindListener() {
-        
 
+        toggleBtn.setOnCheckedChangeListener(new SwitchButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SwitchButton view, boolean isChecked) {
+                //TODO do your job
+                showSorry();
 
+            }
+        });
+
+        layoutRemindSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSorry();
+            }
+        });
+
+        layoutGeneralSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSorry();
+            }
+        });
+        layoutAboutSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSorry();
+            }
+        });
+        layoutHelpSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSorry();
+            }
+        });
+        layoutPrivacySettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSorry();
+            }
+        });
 
         tvSwitchAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,9 +134,15 @@ public class SettingFragment extends Fragment {
         });
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder.unbind();
+    private void showSorry() {
+        mBuilder = new MaterialDialog.Builder(getActivity());
+        mBuilder.title("提醒");
+        mBuilder.titleGravity(GravityEnum.CENTER);
+        mBuilder.titleColor(Color.parseColor("#000000"));
+        mBuilder.content("很抱歉，该功能尚未完成\n敬请期待！");
+        mBuilder.contentColor(Color.parseColor("#000000"));
+        mMaterialDialog = mBuilder.build();
+        mMaterialDialog.show();
+
     }
 }
